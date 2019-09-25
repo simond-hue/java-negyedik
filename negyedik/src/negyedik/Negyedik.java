e79c6e5755d1dc39b66f71d7944c27bde4e66559
simond-hue <achipowa205@gmail.com>
2019-09-25 14:42:11 +0200

    masodik feladat

--- negyedik/src/negyedik/Negyedik.java
+++ negyedik/src/negyedik/Negyedik.java
@@ -5,11 +5,12 @@ import negyedik.ember.Ember;
 public class Negyedik {
 
     public static void main(String[] args) {
-        Ember simonDani = new Ember("Simon Dániel", "1999.04.13", "Budapest");
-        Ember palfalvi = new Ember("Pálfalvi László", "1995.02.11", "Budapest");
-        Ember jakus = new Ember("Jakus Dániel", "1997.10.05", "Budapest");
-        System.out.println(simonDani);
-        System.out.println(palfalvi);
-        System.out.println(jakus);
+        Ember simonDani = new Ember("Simon Dániel", "1999-04-13", "Budapest");
+        Ember palfalvi = new Ember("Pálfalvi László", "1995-02-11", "Budapest");
+        Ember jakus = new Ember("Jakus Dániel", "1997-10-05", "Budapest");
+        System.out.println(simonDani.SzuletesiEv());
+        System.out.println(simonDani.SzulHonap());
+        System.out.println(simonDani.SzulNap());
+        System.out.println(simonDani.Eletkor());
     }
 }
--- negyedik/src/negyedik/ember/Ember.java
+++ negyedik/src/negyedik/ember/Ember.java
@@ -1,5 +1,8 @@
 package negyedik.ember;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 public class Ember {
     private String nev;
     private String szuldatum;
@@ -11,8 +14,26 @@ public class Ember {
         this.szulhely = szulhely;
     }
 
+    public int SzuletesiEv(){
+        return Integer.parseInt(this.szuldatum.substring(0,4));
+    }
+    
+    public int SzulHonap(){
+        return Integer.parseInt(szuldatum.split("-")[1]);
+    }
+    
+    public int SzulNap(){
+        return Integer.parseInt(szuldatum.split("-")[2]);     
+    }
+    
+    public int Eletkor(){
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
+        int year = cal.get(Calendar.YEAR);
+        return year-this.SzuletesiEv();
+    }
+    
     @Override
     public String toString() {
-        return String.format("%s - %s [%s]",this.nev,this.szuldatum,this.szulhely);
+        return String.format("%s: %s [%s]",this.nev,this.szuldatum,this.szulhely);
     }
 }
